88be59720232168523f62ea70fabba9b2678ca0a
Duarte Roso <duarte.roso@posteo.eu>
2021-12-22 18:13:20 +0100

    Remove reference

--- al/buffer.v
+++ al/buffer.v
@@ -75,88 +75,88 @@ pub fn (mut b Buffer) generate() {
 }
 
 // release buffer
-pub fn (b &Buffer) release() {
+pub fn (b Buffer) release() {
 	values := [b.id]
 	C.alDeleteBuffers(values.len, &vopenal.ALuint(values.data))
 	check_error()
 }
 
 // is_valid validates Buffer
-pub fn (b &Buffer) is_valid() bool {
+pub fn (b Buffer) is_valid() bool {
 	ok := C.alIsBuffer(b.id)
 	check_error()
 	return ok == vopenal.al_true
 }
 
 // get_id returns the id of the buffer
-pub fn (b &Buffer) get_id() u32 {
+pub fn (b Buffer) get_id() u32 {
 	return b.id
 }
 
 // get_frequency returns the frequency of the buffer
-pub fn (b &Buffer) get_frequency() int {
+pub fn (b Buffer) get_frequency() int {
 	return b.get_bufferi(vopenal.al_frequency)
 }
 
 // get_bits returns the bits of the buffer
-pub fn (b &Buffer) get_bits() int {
+pub fn (b Buffer) get_bits() int {
 	return b.get_bufferi(vopenal.al_bits)
 }
 
 // get_channels returns the number channels of the buffer
-pub fn (b &Buffer) get_channels() int {
+pub fn (b Buffer) get_channels() int {
 	return b.get_bufferi(vopenal.al_channels)
 }
 
 // get_size returns the size of the buffer
-pub fn (b &Buffer) get_size() int {
+pub fn (b Buffer) get_size() int {
 	return b.get_bufferi(vopenal.al_size)
 }
 
 // set_data sets the data on the buffer
-pub fn (b &Buffer) set_data(format int, data voidptr, size int, frequency f32) {
+pub fn (b Buffer) set_data(format int, data voidptr, size int, frequency f32) {
 	C.alBufferData(b.id, format, data, size, frequency)
 	check_error()
 }
 
 // bufferf sets a buffer parameter value as float
-pub fn (b &Buffer) bufferf(param int, value f32) {
+pub fn (b Buffer) bufferf(param int, value f32) {
 	C.alBufferf(b.id, param, value)
 	check_error()
 }
 
 // buffer3f sets a buffer parameter value as a tuple of floats
-pub fn (b &Buffer) buffer3f(param int, value1 f32, value2 f32, value3 f32) {
+pub fn (b Buffer) buffer3f(param int, value1 f32, value2 f32, value3 f32) {
 	C.alBuffer3f(b.id, param, value1, value2, value3)
 	check_error()
 }
 
 // bufferfv sets a buffer parameter value as a vector of floats
-pub fn (b &Buffer) bufferfv(param int, values []f32) {
+pub fn (b Buffer) bufferfv(param int, values []f32) {
 	C.alBufferfv(b.id, param, values.data)
 	check_error()
 }
 
 // bufferi sets a buffer parameter value as integer
-pub fn (b &Buffer) bufferi(param int, value int) {
+pub fn (b Buffer) bufferi(param int, value int) {
 	C.alBufferi(b.id, param, value)
 	check_error()
 }
 
 // buffer3i sets a buffer parameter value as a tuple of integers
-pub fn (b &Buffer) buffer3i(param int, v1 int, v2 int, v3 int) {
+pub fn (b Buffer) buffer3i(param int, v1 int, v2 int, v3 int) {
 	C.alBuffer3i(b.id, param, v1, v2, v3)
 	check_error()
 }
 
 // bufferiv sets a buffer parameter value as vector of integers
-pub fn (b &Buffer) bufferiv(param int, values []int) {
+pub fn (b Buffer) bufferiv(param int, values []int) {
 	C.alBufferiv(b.id, param, values.data)
 	check_error()
 }
 
 // get_bufferf returns a buffer parameter value as float
-pub fn (b &Buffer) get_bufferf(param int) f32 {
+pub fn (b Buffer) get_bufferf(param int) f32 {
 	mut value := f32(0)
 	C.alGetBufferf(b.id, param, &value)
 	check_error()
@@ -164,7 +164,7 @@ pub fn (b &Buffer) get_bufferf(param int) f32 {
 }
 
 // get_buffer3f returns a buffer parameter value as a tuple of floats
-pub fn (b &Buffer) get_buffer3f(param int) (f32, f32, f32) {
+pub fn (b Buffer) get_buffer3f(param int) (f32, f32, f32) {
 	v1 := f32(0)
 	v2 := f32(0)
 	v3 := f32(0)
@@ -174,7 +174,7 @@ pub fn (b &Buffer) get_buffer3f(param int) (f32, f32, f32) {
 }
 
 // get_bufferfv returns a buffer parameter value as vector of floats
-pub fn (b &Buffer) get_bufferfv(param int) []f32 {
+pub fn (b Buffer) get_bufferfv(param int) []f32 {
 	mut values := []f32{}
 	C.alGetBufferfv(b.id, param, &values.data)
 	check_error()
@@ -182,7 +182,7 @@ pub fn (b &Buffer) get_bufferfv(param int) []f32 {
 }
 
 // get_bufferi returns a buffer parameter value as integer
-pub fn (b &Buffer) get_bufferi(param int) int {
+pub fn (b Buffer) get_bufferi(param int) int {
 	mut value := int(0)
 	C.alGetBufferi(b.id, param, &value)
 	check_error()
@@ -190,7 +190,7 @@ pub fn (b &Buffer) get_bufferi(param int) int {
 }
 
 // get_buffer3i returns a buffer parameter value as a tuple of integers
-pub fn (b &Buffer) get_buffer3i(param int) (int, int, int) {
+pub fn (b Buffer) get_buffer3i(param int) (int, int, int) {
 	v1 := int(0)
 	v2 := int(0)
 	v3 := int(0)
@@ -200,7 +200,7 @@ pub fn (b &Buffer) get_buffer3i(param int) (int, int, int) {
 }
 
 // get_bufferiv returns a buffer parameter value as vector of integers
-pub fn (b &Buffer) get_bufferiv(param int) []int {
+pub fn (b Buffer) get_bufferiv(param int) []int {
 	mut values := []int{}
 	C.alGetBufferiv(b.id, param, &values.data)
 	check_error()
